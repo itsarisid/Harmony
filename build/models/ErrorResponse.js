82b4d94f25d0d50e955e00f7fbbbe1eab7993196
Sajid Khan <khanalex301@gmail.com>
2023-01-08 13:52:48 +0300

    package json update

--- build/models/ErrorResponse.js
+++ build/models/ErrorResponse.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ErrorResponse = void 0;
 class ErrorResponse {
-    constructor(sts) {
-        status: sts;
+    constructor() {
+        this.id = 1;
+        this.statusCode = 500;
+        this.status = "Unexpected Error";
     }
 }
 exports.ErrorResponse = ErrorResponse;
