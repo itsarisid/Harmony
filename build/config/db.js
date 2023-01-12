0c1ddb04b3aec4b36d2e4838b31986918b9340ff
Sajid Khan <khanalex301@gmail.com>
2023-01-12 16:39:24 +0300

    removed build

--- build/config/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const config_1 = require("./config");
-const config = new config_1.Configs();
-const uri = `mongodb+srv://${config.dbCredentials.username}:${config.dbCredentials.password}@harmony.8w14cgj.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-exports.default = client;
